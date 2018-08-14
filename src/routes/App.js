e7a9108f5298c73fe75403ab6754219247a22728
upexcel <rahul.excel2011@gmail.com>
2018-08-14 13:19:31 +0530

    dashboard design

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import Sidemenu from "../components/sidemenu";
+import Dashboard from "../components/dashboard";
+import InnerDashboard from "../components/innerDashboard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Sidemenu>
-          <Route path="/"/>
-          </Sidemenu>
+        <Dashboard>
+          <Route path="/" component={InnerDashboard} />
+        </Dashboard>
       </Router>
     );
   }
